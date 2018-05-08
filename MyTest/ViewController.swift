2f0cf51c2fd0457db50c95c5915ea6ca794246db
useya-mac2 <useya-mac2@useya-mac2-no-imac.local>
2018-05-08 15:35:05 +0900

    Initial Commit

---
+++ MyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTest
+//
+//  Created by useya-mac2 on 2018/05/08.
+//  Copyright © 2018年 useya-mac2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
